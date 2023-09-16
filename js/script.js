1562c9dac5a90e07a165ab3fd48022b4dc96681c
gael.ferreira505@gmail.com <gael.ferreira505@gmail.com>
2023-09-15 21:36:41 -0300

    subindo o projeto finalizado

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
+let listaDeProdutos = [];
+
 function buscarCategorias(){
     fetch("https://fakestoreapi.com/products/categories")
     .then(res => res.json())
-    .then(dados => {
-        dados.forEach(cada => {
+    .then(listaCategorias => {
+
+        listaCategorias.forEach(cada => {
             categorias.innerHTML += `   
                 <li class="list-group-item">
                     <h6>
-                        <input type="checkbox">
+                        <input name="${cada.replace(" ","-")}" type="checkbox" onClick="filtro()">
                         <span>${cada}</span>
                     </h6>
                 </li>`
@@ -16,24 +19,47 @@ function buscarCategorias(){
 
 
 function buscarProdutos(){
-    fetch("https://fakestoreapi.com/products?limit=15")
+    fetch("https://fakestoreapi.com/products")
     .then(res => res.json())
-    .then(dados => {
-        dados.forEach(cada => {
+    .then(listaProdutos => {
+        listaProdutos.forEach(cada => {
+            lista = listaDeProdutos
             products.innerHTML += `
-            <li>
+            <li class="${cada.category.replace(" ","-")}">
                 <div class="card">
-                    <picture>
-                        <img src="${cada.image}" alt="${cada.title}" class="card-img-top">
-                    </picture>
+                    <img src="${cada.image}" class="card-img-top" alt="">
                     <div class="card-body">
-                        <h5 class="card-title">${cada.title}</h5>
-                        <p class="card-text text-warp">${cada.description}</p>
-                        <h6 class="card-text"> $${cada.price} <h6>
-                        <a href="" class="btn btn-primary">Adicionar ao Carrinho</a>
+                        <h5 class="card-title" title="${cada.title}">${cada.title}</h5>
+                        <div class="badge bg-secondary mb-2">${cada.category}</div>
+                        <p class="card-text text-wrap">${cada.description}</p>
+                        <a href="" class="btn btn-primary">Adicionar ao carrinho</a>
                     </div>
                 </div>
-            </li>`
+            </li>
+        `;
         })
     })
 }buscarProdutos()
+
+
+function filtro ()
+{
+    let inputs = document.querySelectorAll('input:checked');
+    let inputs_nomes = [];
+    for(let i = 0; i < inputs.length; i++){
+        inputs_nomes.push(inputs[i].name)
+    }
+
+    let lis = document.querySelectorAll("#products li");
+    for(let i = 0; i < lis.length; i++){
+        if(!lis[i].classList.contains(inputs_nomes.toString())){
+            lis[i].style.display = 'none'
+        }else{
+            lis[i].style.display = 'block' 
+        }
+
+        if(inputs_nomes.length === 0){
+            lis[i].style.display = 'block'
+        }
+    }
+}
